8a7c37ed742f83b205be27c00a903b30729913f9
Mitchell Anderson <mitchellanderson202@hotmail.com>
2023-06-01 16:00:00 +1000

    updated about.tsx

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
         <p>
           I began my development journey in 2020 when I enrolled in a course at
           General Assembly. Since then, I've focused on React.js and Typescript,
-          and more recently, Java & Springboot. You can check out my list of
+          and more recently, Java-SpringBoot & SQL. You can check out my list of
           projects and corresponding technologies to see more.
           <br /> Currently, I'm working as a Trainee Developer through{" "}
           <a href="https://au.nology.io/" target="_blank" rel="noopener">
